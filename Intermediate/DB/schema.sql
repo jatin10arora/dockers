42bb7ac9ea2aeec0a6ae4adad231612ef947f632
unknown <jatin10arora@gmail.com>
2018-03-31 18:11:43 -0700

    Intermediate

---
+++ Intermediate/DB/schema.sql
@@ -0,0 +1,9 @@
+create database test;
+Use test
+CREATE TABLE  user432   
+   (    name VARCHAR(4000),   
+    email VARCHAR(4000),   
+    pass VARCHAR(4000)  
+   );
+
+insert into user432 values ('Demo', 'demo@email.com', 'demopassword');
